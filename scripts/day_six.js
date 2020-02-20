280bd3f9e76ca1ebd4568f104622609c07212761
Oliver Inglis <ollie@theinglistwins.com>
2020-02-20 17:42:54 +0000

    Day Six Completed

--- scripts/day_six.js
+++ scripts/day_six.js
@@ -1,11 +1,25 @@
 const list = document.querySelectorAll('.checker');
 
 let lastChecked
+
 function handleClick(e) {
-  let search = this.getAttribute('name')
-  document.querySelector(`label[for=${search}]`).classList.toggle("done")
+  let search = this.getAttribute('name');
+  document.querySelector(`label[for=${search}]`).classList.toggle("done");
+  let inBetween = false;
+  if(e.shiftKey && this.checked) {
+    list.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        let searchIn = checkbox.getAttribute('name');
+        document.querySelector(`label[for=${searchIn}]`).classList.toggle("done");
+        checkbox.checked = true;
+      }
+    })
+  }
+
   lastChecked = this;
-  console.log(this)
 }
 
 
