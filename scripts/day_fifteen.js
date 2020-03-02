fe3c16775b7eee997158f25e01d4f166fb80b8ad
Oliver Inglis <ollie@theinglistwins.com>
2020-03-02 16:22:20 +0000

    Day Fifteen start"

---
+++ scripts/day_fifteen.js
@@ -0,0 +1,7 @@
+const notepad = document.querySelector('.notepad')
+
+const a = document.createElement('p');
+const b = document.createTextNode('This is the going to be the contents of the text');
+
+
+notepad.appendChild(a);
